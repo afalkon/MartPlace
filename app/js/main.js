db40e60dd13702ca5a3caffbcd3f97e963a5cbc2
afalkon <falconfinance.alex@gmail.com>
2020-05-31 16:48:39 +0200

    Featured Products Section is added

---
+++ app/js/main.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $('.featuredProducts__slider').slick({
+        prevArrow: '<button type="button" class="featuredProducts__slider-prevArrow"><img src="../images/icons/productsSliderChevronPrev.png"</button>',
+        nextArrow: '<button type="button" class="featuredProducts__slider-nextArrow"><img src="../images/icons/productsSliderChevronNext.png"</button>'
+    });
+});
