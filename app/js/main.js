7b83e053a31adbf3a41636884807da14244dad25
afalkon <falconfinance.alex@gmail.com>
2020-06-02 14:06:00 +0200

    Followers Feed Section is added

--- app/js/main.js
+++ app/js/main.js
@@ -3,6 +3,14 @@ $(document).ready(function() {
         prevArrow: '<button type="button" class="featuredProducts__slider-prevArrow"><img src="../images/icons/productsSliderChevronPrev.png"</button>',
         nextArrow: '<button type="button" class="featuredProducts__slider-nextArrow"><img src="../images/icons/productsSliderChevronNext.png"</button>'
     });
+    $('.followersFeed__inner-slider').slick({
+        prevArrow: '<button type="button" class="followersFeed__slider-prevArrow"><img src="../images/icons/productsSliderChevronPrev.png"</button>',
+        nextArrow: '<button type="button" class="followersFeed__slider-nextArrow"><img src="../images/icons/productsSliderChevronNext.png"</button>',
+        slidesToShow: 3,
+        slidesToScroll: 1,
+    });
 });
 
 var mixer = mixitup('.newestProducts__inner-items');
+
+
